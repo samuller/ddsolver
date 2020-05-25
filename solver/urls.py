6504cd88e139cabde16472a3ac2f4354d031c848
Simon Muller <samullers@gmail.com>
2020-05-25 22:14:49 +0200

    Add input form for directory path

--- solver/urls.py
+++ solver/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.main_page, name='home'),
-    url(r'^image.png$', views.main_image, name='image')
+    url(r'^image.png$', views.main_image, name='image'),
+    url(r'^(?P<pathname>.*)$', views.main_page, name='home')
 ]
--- solver/views.py
+++ solver/views.py
@@ -1,15 +1,26 @@
+import os
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
-curr_dir = ''
-curr_img = ''
+curr_dir = None
+curr_img = 'p01.gif'
+
 
 # Create your views here.
-def main_page(request):
-    context = {}
+def main_page(request, pathname):
+    pathname = request.GET.get('pathname', None)
+
+    global curr_dir
+    curr_dir = pathname
+
+    context = {curr_dir: pathname}
     return render(request, 'index.html', context)
 
 def main_image(request):
+    global curr_dir
+    if curr_dir is None or not os.path.exists(curr_dir + curr_img):
+        return HttpResponse('')
+
     import imageio
     im = imageio.imread(curr_dir + curr_img)
 
