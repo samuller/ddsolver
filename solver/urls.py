f411aceee1043f26e55a39307b6438a0dbfd4571
Simon Muller <samullers@gmail.com>
2020-05-25 21:32:26 +0200

    Dynamically load and show image

--- solver/urls.py
+++ solver/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.main_page, name='home')
+    url(r'^$', views.main_page, name='home'),
+    url(r'^image.png$', views.main_image, name='image')
 ]
--- solver/views.py
+++ solver/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+curr_dir = ''
+curr_img = ''
 
 # Create your views here.
 def main_page(request):
     context = {}
     return render(request, 'index.html', context)
+
+def main_image(request):
+    import imageio
+    im = imageio.imread(curr_dir + curr_img)
+
+    response = HttpResponse(content_type="image/png")
+    imageio.imwrite(response, im[:, :], format="png")
+    return response
+
