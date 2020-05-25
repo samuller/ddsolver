1b47e63d58922483b22a942732364b24f21858ab
Simon Muller <samullers@gmail.com>
2020-05-25 22:32:51 +0200

    Add prev/next buttons to traverse folder

--- solver/urls.py
+++ solver/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.main_page, name='home'),
     url(r'^image.png$', views.main_image, name='image'),
-    url(r'^(?P<pathname>.*)$', views.main_page, name='home')
+    url(r'^next_img$', views.next_image, name='next'),
+    url(r'^prev_img$', views.prev_image, name='prev'),
 ]
--- solver/views.py
+++ solver/views.py
@@ -1,19 +1,28 @@
-import os
+import os, glob
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
 curr_dir = None
-curr_img = 'p01.gif'
+curr_img = None
+curr_idx = 0
 
 
 # Create your views here.
-def main_page(request, pathname):
+def main_page(request):
+    global curr_dir, curr_img, curr_idx
     pathname = request.GET.get('pathname', None)
 
-    global curr_dir
-    curr_dir = pathname
+    if pathname is not None:
+        if pathname != curr_dir:
+            curr_idx = 0
+        curr_dir = pathname
 
-    context = {curr_dir: pathname}
+    if curr_dir is not None:
+        images = glob.glob(curr_dir + '*.gif')
+        images = sorted([os.path.basename(img) for img in images])
+        curr_img = images[curr_idx]
+
+    context = {curr_dir: curr_dir, curr_img: curr_img}
     return render(request, 'index.html', context)
 
 def main_image(request):
@@ -27,4 +36,14 @@ def main_image(request):
     response = HttpResponse(content_type="image/png")
     imageio.imwrite(response, im[:, :], format="png")
     return response
+
+def prev_image(request):
+    global curr_idx
+    curr_idx = curr_idx - 1
+    return HttpResponseRedirect('/')
+
+def next_image(request):
+    global curr_idx
+    curr_idx = curr_idx + 1
+    return HttpResponseRedirect('/')
 
